3af7ac8433fdc3dc6920b036f2c4269aab9d38aa
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-22 14:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+var colorSelect = document.getElementById("colorSelect");
+var removeButton = document.querySelector('input[type="button"][value="Select and Remove"]');
+
+
+removeButton.addEventListener('click', function() {
+
+    var selectedIndex = colorSelect.selectedIndex;
+
+
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    }
+});
+
